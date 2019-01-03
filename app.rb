8a5f92e43883d68fb726e1123427c4de80fad87b
Anuj Panchal <anujpan1@gmail.com>
2019-01-03 18:34:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "Hello, World!"
   end
   
+  get '/hometown' do
+  end
+
+  get '/favorite-song' do
+  end
   
 end
